ae2ff27b5806b6a6915cb5a8e3788b7224d36e54
Kristian <344849@via.dk>
2023-11-09 22:17:43 +0100

    Current work on the butterfly code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 //MAKE THE MAGIC HAPPEN
 
 //Butterfly functionality:
+let stopButterflyAnimation = false;
+let butterflyPanicEscape = false;
+setButterflyInitialPosition();
+//$("body").on("mousemove", butterFlyRandomSlowMove);
+$("#butterfly").mouseenter(stop_ButterflyAnimation);
+
+//Butterfly functions:
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -11,36 +18,38 @@ function setButterflyInitialPosition()
     let windowHeight = $(window).height();
     let windowWidth = $(window).width();
     
-
     //Decide randomly if butterfly should come from the left of the screen or right. (or maybe above the screen, depending on the later set yPos)
     if(getRandomInt(10) % 2 === 0)
     {
-        xPos = getRandomInt(windowWidth)+(getRandomInt(windowWidth)/10);
+        xPos = getRandomInt(windowWidth)+(getRandomInt(100));
     }
     else
     {
-        xPos = getRandomInt(windowWidth)-(getRandomInt(windowWidth)/10);
+        xPos = 0-(getRandomInt(100));
     }
 
     let yPos = getRandomInt(windowHeight)-50;
     if(yPos < windowHeight && (xPos < windowWidth || xPos > 0))
     {
-        yPos = yPos-windowHeight;
+        yPos = -400;
     }
 
     $("#butterfly").offset({top: yPos, left: xPos});
-    butterFlyEnterScreen();
+    butterFlyEnterScreen(xPos, yPos);
 }
 
 function butterFlyEnterScreen(xPos, yPos)
 {
-    $("#butterfly").animate({top: yPos, left: xPos}, 15000);
-    butterFlyRandomSlowMove()
+    $("#butterfly").animate({top: yPos, left: xPos}, 2000);
+    butterFlyRandomSlowMove();
 }
 
 
 function butterFlyRandomSlowMove()
 {
+    console.log("Running loop 'Butterfly Random Slow movement'");
+    if(!stopButterflyAnimation)
+    {
     //Define the area of the screen, where the butterfly can fly around in.
     let windowHeight = 100+$(window).height()-200;
     let windowWidth = 100+$(window).width()-200;
@@ -48,9 +57,51 @@ function butterFlyRandomSlowMove()
     let xPos = getRandomInt(windowWidth);
     let yPos = getRandomInt(windowHeight);
 
-    $("#butterfly").animate({top: yPos, left: xPos}, 3500);
-    butterFlyRandomSlowMove()
+    $("#butterfly").animate({top: yPos, left: xPos}, 4000);
+    }
+
+    setTimeout(butterFlyRandomSlowMove, 4000);
 }
 
-setButterflyInitialPosition()
+function butterFlyFastEscape() 
+{
+    if(butterflyPanicEscape)
+    {
+        console.log("Butterfly is in panic!!!!");
+        let windowHeight = $(window).height()-800;
+        let windowWidth = $(window).width()-800;
+    
+        let xPos = getRandomInt(windowWidth);
+        let yPos = getRandomInt(windowHeight);
+        
+        $("#butterfly").animate({top: yPos, left: xPos}, 200);
+    }
+}
+
+function stop_ButterflyAnimation()
+{
+    console.log("Stopping all butterfly animations, and executing panic mode!");
+    $("#butterfly").stop(true);
+    stopButterflyAnimation = true;
+    butterflyPanicEscape = true;
+    butterFlyFastEscape().butterFlyFastEscape().butterFlyFastEscape().butterFlyFastEscape().butterFlyFastEscape().butterFlyFastEscape().butterFlyFastEscape().butterFlyFastEscape();
+    /*butterFlyFastEscape().;
+    setTimeout(butterFlyFastEscape, 400);
+    setTimeout(butterFlyFastEscape, 600);
+    setTimeout(butterFlyFastEscape, 800);
+    setTimeout(butterFlyFastEscape, 1000);
+    setTimeout(butterFlyFastEscape, 1200);
+    setTimeout(butterFlyFastEscape, 1400);
+    setTimeout(butterFlyFastEscape, 1600);
+    setTimeout(butterFlyFastEscape, 1800);
+    setTimeout(butterFlyFastEscape, 2000);*/
+    stopButterflyAnimation = false;
+    butterflyPanicEscape = false;
+}
+
+/*function start_ButterflyAnimation()
+{
+    stopButterflyAnimation = false;
+    butterFlyRandomSlowMove();
+}*/
 
